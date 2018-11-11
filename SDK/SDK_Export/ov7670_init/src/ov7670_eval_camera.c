dbdfb8551490b79122c52c9c51e7dedb8bc4b8b5
thekwan <thekwan@naver.com>
2018-11-11 20:56:12 +0900

    ov7670 initial code update

--- SDK/SDK_Export/ov7670_init/src/ov7670_eval_camera.c
+++ SDK/SDK_Export/ov7670_init/src/ov7670_eval_camera.c
@@ -1,6 +1,5 @@
 #include "ov7670_eval_camera.h"
 #include "sccb.h"
-#include "lcd.h"
 
 
 /*
@@ -13,11 +12,11 @@
 
 struct regval_list ov7670_default_regs[] = {
 	{ REG_COM7, COM7_RESET },
-/*
- * Clock scale: 3 = 15fps
- *              2 = 20fps
- *              1 = 30fps
- */
+	/*
+	 * Clock scale: 3 = 15fps
+	 *              2 = 20fps
+	 *              1 = 30fps
+	 */
 	{ REG_CLKRC, 0x1 },	/* OV: clock scale (30 fps) */
 	{ REG_TSLB,  0x04 },	/* OV */
 	{ REG_COM7, 0 },	/* VGA */
@@ -136,8 +135,6 @@ struct regval_list ov7670_default_regs[] = {
  *
  * IMPORTANT RULE: the first entry must be for COM7, see ov7670_s_fmt for why.
  */
-
-
 struct regval_list ov7670_fmt_yuv422[] = {
 	{ REG_COM7, 0x0 },  /* Selects YUV mode */
 	{ REG_RGB444, 0 },	/* No RGB444 please */
@@ -285,160 +282,8 @@ struct ov7670_win_size ov7670_win_sizes[] = {
 
 
 
-uint32_t dcmi_frame_buff[DCMI_FRAME_BUFF_MAX_SIZE];//	__attribute__ ((section(".bss")));
-
-#ifdef DCMI_DEBUG
-uint32_t dcmi_line_cnt = 0;
-uint32_t dcmi_vsync_cnt = 0;
-uint32_t dcmi_frame_cnt = 0;
-uint32_t dcmi_reg_ndr   = 0;
-uint32_t dcmi_error_code= 0;
-#endif
-
-static DCMI_HandleTypeDef hdcmi_eval;
-static DMA_HandleTypeDef  hdma_eval;
-static CameraResolution current_resolution = 0;
-
-static uint32_t GetSize(CameraResolution resolution);
-
-int dcmi_hw_initialize( void )
-{
-	int ret = 0;
-
-	/*** Configures DCMI peripheral ***/
-	GPIO_InitTypeDef    GPIO_Init_Structure;
-	DCMI_HandleTypeDef *phdcmi;
-
-	/* Enable GPIO clocks */
-	__GPIOA_CLK_ENABLE();
-	__GPIOB_CLK_ENABLE();
-	__GPIOC_CLK_ENABLE();
-	__GPIOE_CLK_ENABLE();
-
-	/* Configure DCMI GPIO as alternate function */
-	GPIO_Init_Structure.Pin       = GPIO_PIN_4 | GPIO_PIN_6 | GPIO_PIN_9 | GPIO_PIN_10;
-	GPIO_Init_Structure.Mode      = GPIO_MODE_AF_OD;
-	GPIO_Init_Structure.Pull      = GPIO_PULLUP;
-	GPIO_Init_Structure.Speed     = GPIO_SPEED_HIGH;
-	GPIO_Init_Structure.Alternate = GPIO_AF13_DCMI;  
-	HAL_GPIO_Init(GPIOA, &GPIO_Init_Structure);
-
-	GPIO_Init_Structure.Pin       = GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9; 
-	GPIO_Init_Structure.Mode      = GPIO_MODE_AF_OD;
-	GPIO_Init_Structure.Pull      = GPIO_PULLUP;
-	GPIO_Init_Structure.Speed     = GPIO_SPEED_HIGH;
-	GPIO_Init_Structure.Alternate = GPIO_AF13_DCMI;   
-	HAL_GPIO_Init(GPIOB, &GPIO_Init_Structure);
-
-	GPIO_Init_Structure.Pin       = GPIO_PIN_8 | GPIO_PIN_9;
-	GPIO_Init_Structure.Mode      = GPIO_MODE_AF_OD;
-	GPIO_Init_Structure.Pull      = GPIO_PULLUP;
-	GPIO_Init_Structure.Speed     = GPIO_SPEED_HIGH;
-	GPIO_Init_Structure.Alternate = GPIO_AF13_DCMI;   
-	HAL_GPIO_Init(GPIOC, &GPIO_Init_Structure);
-
-	GPIO_Init_Structure.Pin       = GPIO_PIN_4;
-	GPIO_Init_Structure.Mode      = GPIO_MODE_AF_OD;
-	GPIO_Init_Structure.Pull      = GPIO_PULLUP;
-	GPIO_Init_Structure.Speed     = GPIO_SPEED_HIGH;
-	GPIO_Init_Structure.Alternate = GPIO_AF13_DCMI;   
-	HAL_GPIO_Init(GPIOE, &GPIO_Init_Structure);
-
-	/* Enable DCMI clock */
-	__DCMI_CLK_ENABLE();
-
-	/* Get the DCMI handle structure */
-	phdcmi = &hdcmi_eval;
-
-	/*** Configures the DCMI to interface with the camera module ***/
-	/* DCMI configuration */
-	phdcmi->Init.CaptureRate      = DCMI_CR_ALL_FRAME;  
-	phdcmi->Init.HSPolarity       = DCMI_HSPOLARITY_LOW;
-	phdcmi->Init.SynchroMode      = DCMI_SYNCHRO_HARDWARE;
-	phdcmi->Init.VSPolarity       = DCMI_VSPOLARITY_HIGH;
-	phdcmi->Init.ExtendedDataMode = DCMI_EXTEND_DATA_8B;
-	phdcmi->Init.PCKPolarity      = DCMI_PCKPOLARITY_RISING;
-	//phdcmi->Init.PCKPolarity      = DCMI_PCKPOLARITY_FALLING;
-	phdcmi->Instance              = DCMI;
-	
-	/*** Configures DMA and NVIC peripherals ***/
-	/* Enable DMA2 clock */
-	__DMA2_CLK_ENABLE(); 
-
-	/*** Configure the DMA ***/
-	/* Set the parameters to be configured */
-	hdma_eval.Init.Channel             = DMA_CHANNEL_1;
-	hdma_eval.Init.Direction           = DMA_PERIPH_TO_MEMORY;
-	hdma_eval.Init.PeriphInc           = DMA_PINC_DISABLE;
-	hdma_eval.Init.MemInc              = DMA_MINC_ENABLE;
-	hdma_eval.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
-	hdma_eval.Init.MemDataAlignment    = DMA_MDATAALIGN_WORD;
-	hdma_eval.Init.Mode                = DMA_CIRCULAR;
-	//hdma_eval.Init.Mode                = DMA_NORMAL;
-	hdma_eval.Init.Priority            = DMA_PRIORITY_HIGH;
-	hdma_eval.Init.FIFOMode            = DMA_FIFOMODE_DISABLE;         
-	hdma_eval.Init.FIFOThreshold       = DMA_FIFO_THRESHOLD_FULL;
-	hdma_eval.Init.MemBurst            = DMA_MBURST_SINGLE;
-	hdma_eval.Init.PeriphBurst         = DMA_PBURST_SINGLE; 
-
-	hdma_eval.Instance = DMA2_Stream1;
-
-	/* Associate the initialized DMA handle to the DCMI handle */
-	__HAL_LINKDMA(phdcmi, DMA_Handle, hdma_eval);
-
-	/*** Configure the NVIC for DCMI and DMA ***/
-	/* NVIC configuration for DCMI transfer complete interrupt */
-	HAL_NVIC_SetPriority(DCMI_IRQn, 5, 0);
-	HAL_NVIC_EnableIRQ(DCMI_IRQn);  
-
-	/* NVIC configuration for DMA2D transfer complete interrupt */
-	HAL_NVIC_SetPriority(DMA2_Stream1_IRQn, 5, 0);
-	HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn);
-
-	/* Configure the DMA stream */
-	if( HAL_DMA_Init(phdcmi->DMA_Handle) != HAL_OK ) {
-		ret = -1;
-	}
-
-	if( HAL_DCMI_Init(phdcmi) != HAL_OK ) {
-		ret = -1;
-	}
-
-#if 0
-	if(ov2640_ReadID(CAMERA_I2C_ADDRESS) == OV2640_ID) { 
-		/* Initialize the camera driver structure */ 
-		camera_drv = &ov2640_drv;     
-		/* Camera Init */   
-		camera_drv->Init(CAMERA_I2C_ADDRESS, Resolution); 
-		/* Return CAMERA_OK status */
-		ret = CAMERA_OK;
-	} 
-  
-  current_resolution = Resolution;
-#else
-#endif
-  BSP_OV7670_Init( );
-
-  current_resolution = CAMERA_R320x240;
-
-#ifdef DCMI_DEBUG
-  /* Initialize debug variables */
-  dcmi_line_cnt = 0;
-  dcmi_vsync_cnt = 0;
-  dcmi_frame_cnt = 0;
-  dcmi_reg_ndr = 0;
-#endif
-  
-  return ret;
-}
-
-void BSP_OV7670_DISPLAY_FRAME( )
-{
-	lcd_copy_RGB565_frameBuffer( dcmi_frame_buff );
-	return;
-}
-
 //Quick hack, approximately 1ms delay
+#if 0
 static void ms_delay(int us)
 {
    while (us-- > 0) {
@@ -449,6 +294,7 @@ static void ms_delay(int us)
          __asm("nop");
    }
 }
+#endif
 
 /*
  * Store a set of start/stop values into the camera.
@@ -457,20 +303,20 @@ int ov7670_set_hw(int hstart, int hstop, int vstart, int vstop)
 {
 	int ret = 0;
 	unsigned char v;
-/*
- * Horizontal: 11 bits, top 8 live in hstart and hstop.  Bottom 3 of
- * hstart are in href[2:0], bottom 3 of hstop in href[5:3].  There is
- * a mystery "edge offset" value in the top two bits of href.
- */
+	/*
+	 * Horizontal: 11 bits, top 8 live in hstart and hstop.  Bottom 3 of
+	 * hstart are in href[2:0], bottom 3 of hstop in href[5:3].  There is
+	 * a mystery "edge offset" value in the top two bits of href.
+	 */
 	sccb_write_reg(REG_HSTART, (hstart >> 3) & 0xff);
 	sccb_write_reg(REG_HSTOP, (hstop >> 3) & 0xff);
 	sccb_read_reg(REG_HREF, &v);
 	v = (v & 0xc0) | ((hstop & 0x7) << 3) | (hstart & 0x7);
 	ms_delay(10);
 	sccb_write_reg(REG_HREF, v);
-/*
- * Vertical: similar arrangement, but only 10 bits.
- */
+	/*
+	 * Vertical: similar arrangement, but only 10 bits.
+	 */
 	sccb_write_reg(REG_VSTART, (vstart >> 2) & 0xff);
 	sccb_write_reg(REG_VSTOP, (vstop >> 2) & 0xff);
 	sccb_read_reg(REG_VREF, &v);
@@ -537,289 +383,4 @@ void BSP_OV7670_Init( )
 	sccb_write_reg( 0x03, 0x04 );   // 
 #endif
 }
-
-int BSP_GET_FRAME_MEM( int addr )
-{
-	return dcmi_frame_buff[addr];
-}
-
-void BSP_SET_FRAME_MEM( int addr , int data )
-{
-	dcmi_frame_buff[addr] = data;
-}
-
-/**
-  * @brief  Starts the camera capture in continuous mode.
-  * @param  buff: pointer to the camera output buffer
-  * @retval None
-  */
-void BSP_CAMERA_ContinuousStart( void )
-{ 
-	/* Start the camera capture */
-	HAL_DCMI_Start_DMA(&hdcmi_eval, DCMI_MODE_CONTINUOUS, (uint32_t)dcmi_frame_buff, GetSize(current_resolution));  
-}
-
-/**
-  * @brief  Starts the camera capture in snapshot mode.
-  * @param  buff: pointer to the camera output buffer
-  * @retval None
-  */
-void BSP_CAMERA_SnapshotStart( void )
-{ 
-	/* Start the camera capture */
-	HAL_DCMI_Start_DMA(&hdcmi_eval, DCMI_MODE_SNAPSHOT, (uint32_t)dcmi_frame_buff, GetSize(current_resolution));  
-}
-
-/**
-  * @brief Suspend the CAMERA capture 
-  * @param  None
-  * @retval None
-  */
-void BSP_CAMERA_Suspend(void) 
-{
-	/* Disable the DMA */
-	__HAL_DMA_DISABLE(hdcmi_eval.DMA_Handle);
-	/* Disable the DCMI */
-	__HAL_DCMI_DISABLE(&hdcmi_eval);
-}
-
-/**
-  * @brief Resume the CAMERA capture 
-  * @param  None
-  * @retval None
-  */
-void BSP_CAMERA_Resume(void) 
-{
-	/* Enable the DCMI */
-	__HAL_DCMI_ENABLE(&hdcmi_eval);
-	/* Enable the DMA */
-	__HAL_DMA_ENABLE(hdcmi_eval.DMA_Handle);
-}
-
-#if 0
-/**
-  * @brief  Stop the CAMERA capture 
-  * @param  None
-  * @retval Camera status
-  */
-uint8_t BSP_CAMERA_Stop(void) 
-{
-	DCMI_HandleTypeDef *phdcmi;
-
-	uint8_t ret = CAMERA_ERROR;
-	
-	/* Get the DCMI handle structure */
-	phdcmi = &hdcmi_eval;
-	
-	if(HAL_DCMI_Stop(phdcmi) == HAL_OK)
-		ret = CAMERA_OK;
-  
-	/* Initialize IO */
-	BSP_IO_Init();
-  
-	/* Reset the camera STANDBY pin */
-	BSP_IO_ConfigPin(XSDN_PIN, IO_MODE_OUTPUT);
-	BSP_IO_WritePin(XSDN_PIN, RESET);  
-  
-	return ret;
-}
-
-/**
-  * @brief  Configures the camera contrast and brightness.
-  * @param  contrast_level: Contrast level
-  *          This parameter can be one of the following values:
-  *            @arg  CAMERA_CONTRAST_LEVEL4: for contrast +2
-  *            @arg  CAMERA_CONTRAST_LEVEL3: for contrast +1
-  *            @arg  CAMERA_CONTRAST_LEVEL2: for contrast  0
-  *            @arg  CAMERA_CONTRAST_LEVEL1: for contrast -1
-  *            @arg  CAMERA_CONTRAST_LEVEL0: for contrast -2
-  * @param  brightness_level: Contrast level
-  *          This parameter can be one of the following values:
-  *            @arg  CAMERA_BRIGHTNESS_LEVEL4: for brightness +2
-  *            @arg  CAMERA_BRIGHTNESS_LEVEL3: for brightness +1
-  *            @arg  CAMERA_BRIGHTNESS_LEVEL2: for brightness  0
-  *            @arg  CAMERA_BRIGHTNESS_LEVEL1: for brightness -1
-  *            @arg  CAMERA_BRIGHTNESS_LEVEL0: for brightness -2    
-  * @retval None
-  */
-void BSP_CAMERA_ContrastBrightnessConfig(uint32_t contrast_level, uint32_t brightness_level)
-{
-	if(camera_drv->Config != NULL)
-  {
-    camera_drv->Config(CAMERA_I2C_ADDRESS, CAMERA_CONTRAST_BRIGHTNESS, contrast_level, brightness_level);
-  }  
-}
-
-/**
-  * @brief  Configures the camera white balance.
-  * @param  Mode: black_white mode
-  *          This parameter can be one of the following values:
-  *            @arg  CAMERA_BLACK_WHITE_BW
-  *            @arg  CAMERA_BLACK_WHITE_NEGATIVE
-  *            @arg  CAMERA_BLACK_WHITE_BW_NEGATIVE
-  *            @arg  CAMERA_BLACK_WHITE_NORMAL       
-  * @retval None
-  */
-void BSP_CAMERA_BlackWhiteConfig(uint32_t Mode)
-{
-  if(camera_drv->Config != NULL)
-  {
-    camera_drv->Config(CAMERA_I2C_ADDRESS, CAMERA_BLACK_WHITE, Mode, 0);
-  }  
-}
-
-/**
-  * @brief  Configures the camera color effect.
-  * @param  Effect: Color effect
-  *          This parameter can be one of the following values:
-  *            @arg  CAMERA_COLOR_EFFECT_ANTIQUE               
-  *            @arg  CAMERA_COLOR_EFFECT_BLUE        
-  *            @arg  CAMERA_COLOR_EFFECT_GREEN    
-  *            @arg  CAMERA_COLOR_EFFECT_RED        
-  * @retval None
-  */
-void BSP_CAMERA_ColorEffectConfig(uint32_t Effect)
-{
-  if(camera_drv->Config != NULL)
-  {
-    camera_drv->Config(CAMERA_I2C_ADDRESS, CAMERA_COLOR_EFFECT, Effect, 0);
-  }  
-}
-#endif
-
-/**
-  * @brief  Handles DCMI interrupt request.
-  * @param  None
-  * @retval None
-  */
-void BSP_DCMI_IRQHandler(void) 
-{
-	HAL_DCMI_IRQHandler(&hdcmi_eval);
-}
-
-/**
-  * @brief  Handles DMA interrupt request.
-  * @param  None
-  * @retval None
-  */
-void BSP_DCMI_DMA_IRQHandler(void) 
-{
-  HAL_DMA_IRQHandler(hdcmi_eval.DMA_Handle);
-}
-
-/**
-  * @brief  Get the capture size.
-  * @param  current_resolution: the current resolution.
-  * @retval capture size.
-  */
-static uint32_t GetSize(CameraResolution resolution)
-{ 
-	uint32_t size = 0;
-
-	/* Get capture size */
-	switch (resolution)
-	{
-		case CAMERA_R176x140:
-			size =  (176 * 140) / PIXEL_PER_4BYTES;
-			break;    
-		case CAMERA_R352x288:
-			size =  (352 * 288) / PIXEL_PER_4BYTES;
-			break;    
-		case CAMERA_R320x240:
-			size =  (320 * 240) / PIXEL_PER_4BYTES;
-			break;
-		case CAMERA_R640x480:
-			size =  (640 * 480) / PIXEL_PER_4BYTES;
-			break;
-		default:
-			break;
-	}
-	return size;
-}
-
-/**
-  * @brief  Initializes the DCMI MSP.
-  * @param  None
-  * @retval None
-  */
-void HAL_DCMI_MspInit(DCMI_HandleTypeDef* hdcmi)
-{
- 
-}
-
-/**
-  * @brief  Line event callback
-  * @param  hdcmi: pointer to the DCMI handle  
-  * @retval None
-  */
-void HAL_DCMI_LineEventCallback(DCMI_HandleTypeDef *hdcmi)
-{        
-	//BSP_LED_Toggle(LED4);
-  //BSP_CAMERA_LineEventCallback();
-#ifdef DCMI_DEBUG
-	dcmi_line_cnt++;
-#endif
-}
-
-/**
-  * @brief  VSYNC event callback
-  * @param  hdcmi: pointer to the DCMI handle  
-  * @retval None
-  */
-void HAL_DCMI_VsyncEventCallback(DCMI_HandleTypeDef *hdcmi)
-{        
-  //BSP_CAMERA_VsyncEventCallback();
-#ifdef DCMI_DEBUG
-	dcmi_vsync_cnt++;
-#endif
-}
-
-/**
-  * @brief  Frame event callback
-  * @param  hdcmi: pointer to the DCMI handle  
-  * @retval None
-  */
-void HAL_DCMI_FrameEventCallback(DCMI_HandleTypeDef *hdcmi)
-{        
-  //BSP_CAMERA_FrameEventCallback();
-	BSP_LED_Toggle(LED4);
-#ifdef DCMI_DEBUG
-	dcmi_frame_cnt++;
-	dcmi_reg_ndr = hdma_eval.Instance->NDTR;
-#endif
-}
-
-/**
-  * @brief  Error callback
-  * @param  hdcmi: pointer to the DCMI handle  
-  * @retval None
-  */
-void HAL_DCMI_ErrorCallback(DCMI_HandleTypeDef *hdcmi)
-{        
-	//BSP_LED_On(LED3);
-	BSP_LED_Toggle(LED3);
-  //BSP_CAMERA_ErrorCallback();
-	dcmi_error_code = hdcmi->ErrorCode;
-}
-
-/**
-  * @}
-  */  
-  
-/**
-  * @}
-  */
-  
-/**
-  * @}
-  */
-  
-/**
-  * @}
-  */      
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
-
-
-
 
--- SDK/SDK_Export/ov7670_init/src/ov7670_eval_camera.h
+++ SDK/SDK_Export/ov7670_init/src/ov7670_eval_camera.h
@@ -1,43 +1,6 @@
 #ifndef _OV7670_EVAL_CAMERA_H_
 #define _OV7670_EVAL_CAMERA_H_
 
-#include "main.h"
-#include "stm32f4xx.h"
-#include "stm32f4xx_hal_dcmi.h"
-
-#define FRAME_BUFF_ROW		240
-#define FRAME_BUFF_COL		320
-#define PIXEL_PER_4BYTES	2
-#define DCMI_FRAME_BUFF_MAX_SIZE	((FRAME_BUFF_ROW * FRAME_BUFF_COL) / PIXEL_PER_4BYTES)
-
-typedef enum {
-	CAMERA_R176x140,	// QCIF
-	CAMERA_R352x288,	// CIF
-	CAMERA_R320x240,	// QVGA
-	CAMERA_R640x480 	// VGA
-}CameraResolution ;
-
-#ifdef DCMI_DEBUG
-extern uint32_t dcmi_line_cnt;
-extern uint32_t dcmi_vsync_cnt;
-extern uint32_t dcmi_frame_cnt;
-extern uint32_t dcmi_reg_ndr;
-extern uint32_t dcmi_error_code;
-#endif
-
-int   dcmi_hw_initialize( void );
-int   BSP_GET_FRAME_MEM ( int addr );
-void  BSP_SET_FRAME_MEM ( int addr, int data );
-void  BSP_CAMERA_ContinuousStart( void );
-void  BSP_CAMERA_SnapshotStart  ( void );
-void  BSP_CAMERA_Suspend        ( void );
-void  BSP_CAMERA_Resume         ( void );
-//void  BSP_CAMERA_Stop           ( void );
-void  BSP_DCMI_IRQHandler       ( void );
-void  BSP_DCMI_DMA_IRQHandler   ( void );
-void  BSP_OV7670_Init           ( void );
-void  BSP_OV7670_DISPLAY_FRAME  ( void );
-
 
 /*
  * The 7670 sits on i2c with ID 0x42
